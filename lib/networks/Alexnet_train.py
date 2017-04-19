8f6b1fe6fb7de5944e30b96f3cac97942ae93b81
GBY <gaoboyang@gmail.com>
2017-04-19 23:20:21 +0200

    add lrn layer but conv layer is wrong because of group argument （used for splitting data and filters）

--- lib/networks/Alexnet_train.py
+++ lib/networks/Alexnet_train.py
@@ -32,53 +32,55 @@ class Alexnet_train(Network):
 
     def setup(self):
         (self.feed('data')
-             .conv(11, 11, 64, 4, 4, name='alexnet_v2/conv1', trainable=True, padding='VALID')
-             .max_pool(3, 3, 2, 2, padding='VALID', name='alexnet_v2/pool1')
-             .conv(5, 5, 192, 1, 1, name='alexnet_v2/conv2', trainable=True)
-             .max_pool(3, 3, 2, 2, padding='VALID', name='alexnet_v2/pool2')
-             .conv(3, 3, 384, 1, 1, name='alexnet_v2/conv3')
-             .conv(3, 3, 384, 1, 1, name='alexnet_v2/conv4')
-             .conv(3, 3, 256, 1, 1, name='alexnet_v2/conv5')
-             .max_pool(3, 3, 2, 2, padding='VALID', name='alexnet_v2/pool5'))
+            .conv(11, 11, 96, 4, 4, name='conv1', trainable=False, padding='SAME')
+            .lrn(radius=2, alpha=2e-05, beta=0.75, name='lrn1', bias=1.0)
+            .max_pool(3, 3, 2, 2, padding='VALID', name='pool1')
+            .conv(5, 5, 256, 1, 1, name='conv2', trainable=False, padding='SAME')
+            .lrn(radius=2, alpha=2e-05, beta=0.75, name='lrn2', bias=1.0)
+            .max_pool(3, 3, 2, 2, padding='VALID', name='pool2')
+            .conv(3, 3, 384, 1, 1, name='conv3', trainable=False, padding='SAME')
+            .conv(3, 3, 384, 1, 1, name='conv4', trainable=False, padding='SAME')
+            .conv(3, 3, 256, 1, 1, name='conv5', trainable=False, padding='SAME')
+            .max_pool(3, 3, 2, 2, padding='VALID', name='pool5'))
 
         #========= RPN ============
-        (self.feed('alexnet_v2/pool5')
-             .conv(3,3,512,1,1,name='rpn_conv/3x3')
-             .conv(1,1,len(anchor_scales)*3*2 ,1 , 1, padding='VALID', relu = False, name='rpn_cls_score'))
+        (self.feed('pool5')
+            .conv(3,3,512,1,1,name='rpn_conv/3x3')
+            .conv(1,1,len(anchor_scales)*3*2 ,1 , 1, padding='VALID', relu = False, name='rpn_cls_score'))
 
         (self.feed('rpn_cls_score','gt_boxes','im_info','data')
-             .anchor_target_layer(_feat_stride, anchor_scales, name = 'rpn-data' ))
+            .anchor_target_layer(_feat_stride, anchor_scales, name = 'rpn-data' ))
 
         # Loss of rpn_cls & rpn_boxes
 
         (self.feed('rpn_conv/3x3')
-             .conv(1,1,len(anchor_scales)*3*4, 1, 1, padding='VALID', relu = False, name='rpn_bbox_pred'))
+            .conv(1,1,len(anchor_scales)*3*4, 1, 1, padding='VALID', relu = False, name='rpn_bbox_pred'))
 
         #========= RoI Proposal ============
         (self.feed('rpn_cls_score')
-             .reshape_layer(2,name = 'rpn_cls_score_reshape')
-             .softmax(name='rpn_cls_prob'))
+            .reshape_layer(2,name = 'rpn_cls_score_reshape')
+            .softmax(name='rpn_cls_prob'))
 
         (self.feed('rpn_cls_prob')
-             .reshape_layer(len(anchor_scales)*3*2,name = 'rpn_cls_prob_reshape'))
+            .reshape_layer(len(anchor_scales)*3*2,name = 'rpn_cls_prob_reshape'))
 
         (self.feed('rpn_cls_prob_reshape','rpn_bbox_pred','im_info')
-             .proposal_layer(_feat_stride, anchor_scales, 'TRAIN',name = 'rpn_rois'))
+            .proposal_layer(_feat_stride, anchor_scales, 'TRAIN',name = 'rpn_rois'))
 
         (self.feed('rpn_rois','gt_boxes')
-             .proposal_target_layer(n_classes,name = 'roi-data'))
+            .proposal_target_layer(n_classes,name = 'roi-data'))
 
 
         #========= RCNN ============
-        (self.feed('alexnet_v2/pool5', 'roi-data')
-             .roi_pool(7, 7, 1.0/16, name='pool_5')
-             .fc(4096, name='alexnet_v2/fc6')
-             .dropout(0.5, name='drop6')
-             .fc(4096, name='alexnet_v2/fc7')
-             .dropout(0.5, name='drop7')
-             .fc(n_classes, relu=False, name='cls_score')
-             .softmax(name='cls_prob'))
+        (self.feed('pool5', 'roi-data')
+            .roi_pool(7, 7, 1.0/16, name='pool_5')
+            .fc(4096, name='fc6')
+            .dropout(0.5, name='drop6')
+            .fc(4096, name='fc7')
+            .dropout(0.5, name='drop7')
+            .fc(n_classes, relu=False, name='cls_score')
+            .softmax(name='cls_prob'))
 
         (self.feed('drop7')
-             .fc(n_classes*4, relu=False, name='bbox_pred'))
+            .fc(n_classes*4, relu=False, name='bbox_pred'))
 
